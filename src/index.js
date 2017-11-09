0035d0d31b0bcae2caba0daea13fa42b8eb09c70
Terry Tilley <terry@terrytilley.com>
2017-11-09 21:44:32 +0000

    Setting Up React with GraphQL

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  ApolloClient,
+  ApolloProvider,
+  createNetworkInterface,
+} from 'react-apollo';
+
+import Routes from './routes';
+import registerServiceWorker from './registerServiceWorker';
+
+const networkInterface = createNetworkInterface({
+  uri: 'http://localhost:8000/graphql',
+});
+
+const client = new ApolloClient({
+  networkInterface,
+});
+
+const App = (
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>
+);
+
+ReactDOM.render(App, document.getElementById('root'));
+registerServiceWorker();
