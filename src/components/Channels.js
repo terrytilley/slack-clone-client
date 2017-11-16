bc77bb08c8ec790e97d1970ceec0644b5faa6607
Terry Tilley <terry@terrytilley.com>
2017-11-16 20:16:51 +0000

    Building Slack's Sidebar with Styled Components

--- src/components/Channels.js
+++ src/components/Channels.js
@@ -4,27 +4,73 @@ import styled from 'styled-components';
 const ChannelWrapper = styled.div`
   grid-column: 2;
   grid-row: 1 / 4;
-  color: #958993;
   background-color: #4e3a4c;
+  color: #958993;
 `;
 
-const user = ({ id, name }) => <li key={`user-${id}`}>{name}</li>;
-const channel = ({ id, name }) => <li key={`channel-${id}`}>{`# ${name}`}</li>;
+const TeamNameHeader = styled.h1`
+  color: #fff;
+  font-size: 20px;
+`;
+
+const SideBarList = styled.ul`
+  width: 100%;
+  list-style: none;
+  padding-left: 0px;
+`;
+
+const paddingLeft = 'padding-left: 10px';
+
+const SideBarListItem = styled.li`
+  padding: 2px;
+  ${paddingLeft};
+  &:hover {
+    background: #3e313c;
+  }
+`;
+
+const SideBarListHeader = styled.li`
+  ${paddingLeft};
+`;
+
+const PushLeft = styled.div`
+  ${paddingLeft};
+`;
+
+const Green = styled.span`
+  color: #38978d;
+`;
+
+const Bubble = ({ on = true }) => (on ? <Green>●</Green> : '○');
+
+const channel = ({ id, name }) => (
+  <SideBarListItem key={`channel-${id}`}># {name}</SideBarListItem>
+);
+
+const user = ({ id, name }) => (
+  <SideBarListItem key={`user-${id}`}>
+    <Bubble /> {name}
+  </SideBarListItem>
+);
 
 const Channels = ({ teamName, username, channels, users }) => (
   <ChannelWrapper>
-    <div>
-      {teamName}
+    <PushLeft>
+      <TeamNameHeader>{teamName}</TeamNameHeader>
       {username}
+    </PushLeft>
+    <div>
+      <SideBarList>
+        <SideBarListHeader>Channels</SideBarListHeader>
+        {channels.map(channel)}
+      </SideBarList>
     </div>
-    <ul>
-      <li>Channels</li>
-      {channels.map(channel)}
-    </ul>
-    <ul>
-      <li>Direct Messages</li>
-      {users.map(user)}
-    </ul>
+    <div>
+      <SideBarList>
+        <SideBarListHeader>Direct Messages</SideBarListHeader>
+        {users.map(user)}
+      </SideBarList>
+    </div>
   </ChannelWrapper>
 );
 
