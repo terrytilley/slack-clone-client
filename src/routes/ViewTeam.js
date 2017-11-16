871f2750419afa86e0e9d85f929c5be2076208d8
Terry Tilley <terry@terrytilley.com>
2017-11-16 21:40:27 +0000

    Building Container Component and GraphQL Query

--- src/routes/ViewTeam.js
+++ src/routes/ViewTeam.js
@@ -1,26 +1,19 @@
 import React from 'react';
 
-import Teams from '../components/Teams';
 import Header from '../components/Header';
-import Channels from '../components/Channels';
 import Messages from '../components/Messages';
-import AppLayout from '../components/AppLayout';
 import SendMessage from '../components/SendMessage';
+import AppLayout from '../components/AppLayout';
+import Sidebar from '../containers/Sidebar';
 
 const ViewTeam = () => (
   <AppLayout>
-    <Teams teams={[{ id: 1, letter: 'M' }, { id: 2, letter: 'T' }]} />
-    <Channels
-      teamName="Team Name"
-      username="username"
-      channels={[{ id: 1, name: 'general' }, { id: 2, name: 'random' }]}
-      users={[{ id: 1, name: 'slackbot' }, { id: 2, name: 'user1' }]}
-    />
+    <Sidebar currentTeamId={1} />
     <Header channelName="general" />
     <Messages>
       <ul className="message-list">
-        <li>Message 1</li>
-        <li>Message 2</li>
+        <li />
+        <li />
       </ul>
     </Messages>
     <SendMessage channelName="general" />
